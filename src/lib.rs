2bf5be0a0dc9c4bf14b6d2b3c93ba5da6cae89a0
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-05-09 14:27:42 +0300

    Added crate-level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! # mtzip
+//!
+//! MTZIP (Stand for Multi-Threaded ZIP) is a library for making zip archives utilising all the threads of the machine.
+//!
+//! Example usage:
+//!
+//! ```ignore
+//! use mtzip::ZipArchive;
+//!
+//! // Creating the zipper that holds data and handles compression
+//! let zipper = ZipArchive::default();
+//!
+//! // Adding a file from filesystem
+//! zipper.add_file("input/test_text_file.txt", "test_text_file.txt");
+//!
+//! // Adding a file from a byte array
+//! zipper.add_file_from_slice(b"Hello, world!", "hello_world.txt");
+//!
+//! // Adding a directory and a file to it
+//! zipper.add_directory("test_dir");
+//! // And adding a file to it
+//! zipper.add_file("input/file_that_goes_to_a_dir.txt", "test_dir/file_that_goes_to_a_dir.txt");
+//!
+//! // Writing to a file
+//! // First, open the file
+//! let mut file = File::create("output.zip").unwrap();
+//! // Then, write to it
+//! zipper.write(&mut file); // Amount of threads is chosen automatically
+//! ```
+
 use flate2::{read::DeflateEncoder, Compression, CrcReader};
 use std::{
     cell::Cell,
