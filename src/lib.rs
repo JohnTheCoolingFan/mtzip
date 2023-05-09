a8ddfad164e91b27bebdc9ac7377fb9c27088683
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-05-09 14:18:45 +0300

    Switch to ToString generic for names

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ pub struct ZipArchive<'a> {
 
 impl<'a> ZipArchive<'a> {
     /// Add file from filesystem. Will read on compression.
-    pub fn add_file(&self, fs_path: impl Into<PathBuf>, archive_name: &str) {
+    pub fn add_file(&self, fs_path: impl Into<PathBuf>, archive_name: impl ToString) {
         self.compressed.set(false);
         let name = archive_name.to_string();
         let job = ZipJob {
@@ -50,7 +50,7 @@ impl<'a> ZipArchive<'a> {
     /// problems with lifetimes, as the reference must be valid throughout the whoel existence of
     /// [`Self`]. This can be avoided using
     /// [`add_file_from_owned_data`](Self::add_file_from_owned_data) instead.
-    pub fn add_file_from_slice(&self, data: &'a [u8], archive_name: &str) {
+    pub fn add_file_from_slice(&self, data: &'a [u8], archive_name: impl ToString) {
         self.compressed.set(false);
         let data = data;
         let name = archive_name.to_string();
@@ -66,7 +66,7 @@ impl<'a> ZipArchive<'a> {
 
     /// Add file from an owned data source. Data is stored in archive struct for later compression.
     /// Helps avoiding lifetime hell at the cost of allocation in soem cases.
-    pub fn add_file_from_owned_data(&self, data: impl Into<Vec<u8>>, archive_name: &str) {
+    pub fn add_file_from_owned_data(&self, data: impl Into<Vec<u8>>, archive_name: impl ToString) {
         self.compressed.set(false);
         let name = archive_name.to_string();
         let job = ZipJob {
@@ -80,7 +80,7 @@ impl<'a> ZipArchive<'a> {
     }
 
     /// Add a directory entry
-    pub fn add_directory(&self, archive_name: &str) {
+    pub fn add_directory(&self, archive_name: impl ToString) {
         self.compressed.set(false);
         let name = archive_name.to_string();
         let job = ZipJob {
