58a4f106b1979163bf7cf4c04552a8df69972816
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-03-24 07:21:03 +0300

    Some ZipJob refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -215,6 +215,45 @@ struct ZipJob<'a> {
 }
 
 impl ZipJob<'_> {
+    fn file_attributes(file: &File) -> std::io::Result<u32> {
+        let metadata = file.metadata()?;
+        cfg_if! {
+            if #[cfg(target_os = "windows")] {
+                Ok(metadata.file_attributes())
+            } else if #[cfg(target_os = "linux")] {
+                use std::os::linux::fs::MetadataExt;
+                Ok(metadata.st_mode())
+            } else if #[cfg(target_os = "unix")] {
+                Ok(metadata.permissions().mode())
+            } else {
+                Ok(0o100644 << 16)
+            }
+        }
+    }
+
+    fn gen_file<R: Read>(
+        source: R,
+        uncompressed_size: u32,
+        archive_path: String,
+        attributes: Option<u32>,
+    ) -> ZipFile {
+        let crc_reader = CrcReader::new(source);
+        let mut encoder = DeflateEncoder::new(crc_reader, Compression::new(9));
+        let mut data = Vec::with_capacity(uncompressed_size as usize);
+        encoder.read_to_end(&mut data).unwrap();
+        data.shrink_to_fit();
+        let crc_reader = encoder.into_inner();
+        let crc = crc_reader.crc().sum();
+        ZipFile {
+            compression_type: CompressionType::Deflate,
+            crc,
+            uncompressed_size,
+            filename: archive_path,
+            data,
+            external_file_attributes: attributes.unwrap_or(0),
+        }
+    }
+
     fn into_file(self) -> ZipFile {
         match self.data_origin {
             ZipJobOrigin::Directory => ZipFile::directory(self.archive_path),
@@ -222,70 +261,26 @@ impl ZipJob<'_> {
                 let file = File::open(fs_path).unwrap();
                 let file_metadata = file.metadata().unwrap();
                 let uncompressed_size = file_metadata.len() as u32;
-                cfg_if! {
-                    if #[cfg(target_os = "windows")] {
-                        let external_file_attributes = file_metadata.file_attributes();
-                    } else if #[cfg(target_os = "unix")] {
-                        let external_file_attributes = file_metadata.permissions().mode();
-                    } else if #[cfg(target_os = "linux")] {
-                        use std::os::linux::fs::MetadataExt;
-                        let external_file_attributes = file_metadata.st_mode();
-                    } else {
-                        let external_file_attributes = 0o100644 << 16;
-                    }
-                }
-                let crc_reader = CrcReader::new(file);
-                let mut encoder = DeflateEncoder::new(crc_reader, Compression::new(9));
-                let mut data = Vec::with_capacity(uncompressed_size as usize);
-                encoder.read_to_end(&mut data).unwrap();
-                data.shrink_to_fit();
-                let crc_reader = encoder.into_inner();
-                let crc = crc_reader.crc().sum();
-
-                ZipFile {
-                    compression_type: CompressionType::Deflate,
-                    crc,
+                let external_file_attributes = Self::file_attributes(&file).unwrap();
+                Self::gen_file(
+                    file,
                     uncompressed_size,
-                    filename: self.archive_path,
-                    data,
-                    external_file_attributes,
-                }
+                    self.archive_path,
+                    Some(external_file_attributes),
+                )
             }
             ZipJobOrigin::RawData(in_data) => {
                 let uncompressed_size = in_data.len() as u32;
-                let crc_reader = CrcReader::new(in_data);
-                let mut encoder = DeflateEncoder::new(crc_reader, Compression::new(9));
-                let mut data = Vec::with_capacity(uncompressed_size as usize);
-                encoder.read_to_end(&mut data).unwrap();
-                data.shrink_to_fit();
-                let crc_reader = encoder.into_inner();
-                let crc = crc_reader.crc().sum();
-                ZipFile {
-                    compression_type: CompressionType::Deflate,
-                    crc,
-                    uncompressed_size,
-                    filename: self.archive_path,
-                    data,
-                    external_file_attributes: 0,
-                }
+                Self::gen_file(in_data, uncompressed_size, self.archive_path, None)
             }
             ZipJobOrigin::RawDataOwned(in_data) => {
                 let uncompressed_size = in_data.len() as u32;
-                let crc_reader = CrcReader::<&[u8]>::new(in_data.as_ref());
-                let mut encoder = DeflateEncoder::new(crc_reader, Compression::new(9));
-                let mut data = Vec::with_capacity(uncompressed_size as usize);
-                encoder.read_to_end(&mut data).unwrap();
-                data.shrink_to_fit();
-                let crc_reader = encoder.into_inner();
-                let crc = crc_reader.crc().sum();
-                ZipFile {
-                    compression_type: CompressionType::Deflate,
-                    crc,
+                Self::gen_file::<&[u8]>(
+                    in_data.as_ref(),
                     uncompressed_size,
-                    filename: self.archive_path,
-                    data,
-                    external_file_attributes: 0,
-                }
+                    self.archive_path,
+                    None,
+                )
             }
         }
     }
