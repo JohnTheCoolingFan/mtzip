fffc5c4ee6f00add25ccd5cbb8c7eafa86dd8845
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-03-27 00:22:25 +0300

    Move CompressionLevel to a separate module

--- src/lib.rs
+++ src/lib.rs
@@ -33,8 +33,6 @@
 #[cfg(target_os = "windows")]
 use std::os::windows::fs::MetadataExt;
 use std::{
-    error::Error,
-    fmt::Display,
     fs::File,
     io::{Read, Seek, Write},
     num::NonZeroUsize,
@@ -44,6 +42,8 @@ use std::{
 
 use cfg_if::cfg_if;
 use flate2::{read::DeflateEncoder, Compression, CrcReader};
+
+pub mod level;
 
 const VERSION_NEEDED_TO_EXTRACT: u16 = 20;
 #[cfg(not(target_os = "windows"))]
@@ -67,126 +67,6 @@ const GENERAL_PURPOSE_BIT_FLAG: u16 = 1 << 11;
 pub enum CompressionType {
     Stored = 0,
     Deflate = 8,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct CompressionLevel(u8);
-
-impl CompressionLevel {
-    /// Construct a new value of a compression level setting.
-    ///
-    /// The integer value must be less than or equal to 9, otherwise `None` is returned
-    #[inline]
-    pub const fn new(level: u8) -> Option<Self> {
-        if level <= 9 {
-            Some(Self(level))
-        } else {
-            None
-        }
-    }
-
-    /// Construct a new value of a compression level setting without checking the value.
-    ///
-    /// # Safety
-    ///
-    /// The value must be a valid supported compression level
-    #[inline]
-    pub const unsafe fn new_unchecked(level: u8) -> Self {
-        Self(level)
-    }
-
-    /// No compression
-    #[inline]
-    pub const fn none() -> Self {
-        Self(0)
-    }
-
-    /// Fastest compression
-    #[inline]
-    pub const fn fast() -> Self {
-        Self(1)
-    }
-
-    /// Balanced level with moderate compression and speed. The raw value is 6.
-    #[inline]
-    pub const fn balanced() -> Self {
-        Self(6)
-    }
-
-    /// Best compression ratio, comes at a worse performance
-    #[inline]
-    pub const fn best() -> Self {
-        Self(9)
-    }
-
-    /// Get the compression level as an integer
-    #[inline]
-    pub const fn get(self) -> u8 {
-        self.0
-    }
-}
-
-impl Default for CompressionLevel {
-    /// Equivalent to [`Self::balanced`]
-    fn default() -> Self {
-        Self::balanced()
-    }
-}
-
-/// The number for compression level was invalid
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct InvalidCompressionLevel(u32);
-
-impl InvalidCompressionLevel {
-    /// The value which was supplied
-    pub fn value(self) -> u32 {
-        self.0
-    }
-}
-
-impl Display for InvalidCompressionLevel {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Invalid compression level number: {}", self.0)
-    }
-}
-
-impl Error for InvalidCompressionLevel {}
-
-impl From<CompressionLevel> for Compression {
-    #[inline]
-    fn from(value: CompressionLevel) -> Self {
-        Compression::new(value.0.into())
-    }
-}
-
-impl TryFrom<Compression> for CompressionLevel {
-    type Error = InvalidCompressionLevel;
-
-    fn try_from(value: Compression) -> Result<Self, Self::Error> {
-        let level = value.level();
-        Self::new(
-            level
-                .try_into()
-                .map_err(|_| InvalidCompressionLevel(level))?,
-        )
-        .ok_or(InvalidCompressionLevel(level))
-    }
-}
-
-impl From<CompressionLevel> for u8 {
-    #[inline]
-    fn from(value: CompressionLevel) -> Self {
-        value.0
-    }
-}
-
-impl TryFrom<u8> for CompressionLevel {
-    type Error = InvalidCompressionLevel;
-
-    #[inline]
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        Self::new(value).ok_or(InvalidCompressionLevel(value.into()))
-    }
 }
 
 /// Initialize using [`Default`] trait implementation. Uses interior mutabillity for inner state
